a775240da8b08cbb40e325cee9de99aee5ed3574
Thanush Babu <tbabu@fleetstudio.com>
2026-02-12 14:21:23 +0530

    Chore : Template Structure Fixes and  Loading

--- modules/helper/index.js
+++ modules/helper/index.js
@@ -6,17 +6,27 @@ module.exports = {
     self.addHelpers({
       linkPath: (link) => {
         if (!link) {
-          return;
+          return '#';
         }
-        let path;
-        if (link.linkType === 'page' && link._linkPage && link._linkPage[0]) {
+        let path = '#';
+        if (link.linkType === 'page' && link._linkPage && link._linkPage[0] && link._linkPage[0]._url) {
           path = link._linkPage[0]._url;
-        } else if (link.linkType === 'file' && link._linkFile && link._linkFile[0]) {
+        } else if (link.linkType === 'file' && link._linkFile && link._linkFile[0] && link._linkFile[0]._url) {
           path = link._linkFile[0]._url;
-        } else if (link.linkType === 'custom') {
+        } else if (link.linkType === 'custom' && link.linkUrl) {
           path = link.linkUrl;
         }
         return path;
+      },
+      linkTarget: (link) => {
+        if (!link) {
+          return '_self';
+        }
+        let target = '_self';
+        if (link.linkTarget && Array.isArray(link.linkTarget) && link.linkTarget.length && link.linkTarget[0] === '_blank') {
+          target = '_blank';
+        }
+        return target;
       }
     });
   }
